7cc17dbc8aca40f4f08490761a4631d0c208dce5
Muhammad Shafie <shafiehb@gmail.com>
2024-03-02 02:23:21 +0800

    first commit megcom-dynamic navbar

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import navbar from './components/small-components/navbar.vue'
+</script>
+
+<template>
+  <navbar />
+  <RouterView />
+</template>
+
+<style scoped></style>
